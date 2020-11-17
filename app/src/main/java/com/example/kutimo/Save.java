d480673abee592ec9df8390b334be5bd65a7fb8e
shakerbaker78 <bakeamym@yahoo.com>
2020-11-17 13:49:20 -0700

    Shared Preferences and unlock images prototypes

---
+++ app/src/main/java/com/example/kutimo/Save.java
@@ -0,0 +1,43 @@
+package com.example.sharedpreferencesprototype;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class Save {
+    private MainActivity mainActivity;
+    private int faithPoints;
+    private static final String TAG = "Shared Preferences";
+    SharedPreferences sharedPreferences;
+
+    /** Constructor **/
+    public Save(MainActivity mainActivity) {
+
+        this.mainActivity = mainActivity;
+        sharedPreferences = this.mainActivity.getSharedPreferences("com.example.sharedpreferencesprototype", Context.MODE_PRIVATE);
+        faithPoints = 0;
+    }
+
+
+
+    void saveFaithPoints(int faithPoints){
+        sharedPreferences.edit().putInt("faithPoints", faithPoints).apply();
+        Log.i(TAG, "faith points: " + faithPoints);
+
+    }
+
+    int retrieveFaithPoints(){
+        faithPoints = sharedPreferences.getInt("faithPoints", faithPoints);
+        Log.i(TAG, "faith points: " + faithPoints);
+        return faithPoints;
+
+
+    }
+
+    // To save the scriptures I need to parse the arraylist to a json file and then read it to the saver as a string
+    // To return the scriptures we need to get the string from saved preferences and parse it to the array list in the data class.
+
+
+
+
+}
