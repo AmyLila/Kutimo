98715d4a907be44fea51a32b0a196026c666c4d8
timjo6789 <tgj2u2@gmail.com>
2020-11-18 16:24:55 -0700

    adding intent (part 2)

--- app/src/main/java/com/example/kutimo/intent.java
+++ app/src/main/java/com/example/kutimo/intent.java
@@ -1,21 +1,72 @@
 package com.example.kutimo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class intent extends WearableActivity {
+import java.util.ArrayList;
 
-    private TextView mTextView;
+public class intent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
 
-        // Enables Always-on
-        setAmbientEnabled();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            } else if (type.startsWith("image/")) {
+                handleSendImage(intent); // Handle single image being sent
+            }
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendMultipleImages(intent); // Handle multiple images being sent
+            }
+        } else {
+            // Handle other intents, such as being started from the home screen
+            Toast.makeText(this, "Not working.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            Toast.makeText(this, sharedText, Toast.LENGTH_SHORT).show();
+            EditText textView = (EditText) findViewById(R.id.intentResult);
+            textView.setText(sharedText);
+        }
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            Toast.makeText(this, "URI Image", Toast.LENGTH_SHORT).show();
+            // Update UI to reflect image being shared
+        }
+    }
+
+    void handleSendMultipleImages(Intent intent) {
+        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (imageUris != null) {
+            Toast.makeText(this, "Multiple Images.", Toast.LENGTH_SHORT).show();
+            // Update UI to reflect multiple images being shared
+        }
     }
 }
