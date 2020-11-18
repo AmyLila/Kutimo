87bb8edcfe59fd3f762a9123ab0ffdd2fd10cab6
MeganDLS <megan.siguenza@gmail.com>
2020-11-18 11:30:02 -0700

    Adding calendar activity and colors (light_blue and dark_blue.)

---
+++ app/src/main/java/com/example/kutimo/Calendar_checkmarks.java
@@ -0,0 +1,14 @@
+package com.example.kutimo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calendar_checkmarks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_checkmarks);
+    }
+}
