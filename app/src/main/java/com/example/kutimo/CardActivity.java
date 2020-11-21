bff5d44ad8bf02ca4de5b05f0aadb0ea0e7ec8d3
shakerbaker78 <bakeamym@yahoo.com>
2020-11-20 18:12:39 -0700

    working on the card activity layout

--- app/src/main/java/com/example/kutimo/CardActivity.java
+++ app/src/main/java/com/example/kutimo/CardActivity.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 public class CardActivity extends AppCompatActivity {
 
     private static final String TAG = "Card Activity";
-    private int faithPoints, level1, level2, level3;
-    ImageView image_level1, image_level2, image_level3;
+    private int faithPoints, level1, level2, level3, level4, level5, level6, level7, level8, level9, level10, level11, level12;
+    ImageView image_level1, image_level2, image_level3, image_level4, image_level5, image_level6, image_level7, image_level8, image_level9, image_level10;
     Save save;
 
 
@@ -24,16 +24,44 @@ public class CardActivity extends AppCompatActivity {
         image_level1 = (ImageView) findViewById(R.id.image_level1);
         image_level2 = (ImageView) findViewById(R.id.image_level2);
         image_level3 = (ImageView) findViewById(R.id.image_level3);
+        image_level4 = (ImageView) findViewById(R.id.image_level4);
+        image_level5 = (ImageView) findViewById(R.id.image_level5);
+        image_level6 = (ImageView) findViewById(R.id.image_level6);
+        image_level7 = (ImageView) findViewById(R.id.image_level7);
+        image_level8 = (ImageView) findViewById(R.id.image_level8);
+        image_level9 = (ImageView) findViewById(R.id.image_level9);
+        image_level10 = (ImageView) findViewById(R.id.image_level10);
 
         //Trial images aren't working in drawable
         if (faithPoints >= level1){
-            image_level1.setImageResource(R.drawable.unlocked_level1);
+            image_level1.setImageResource(R.drawable.abel);
         }
         if (faithPoints >= level2){
-            image_level1.setImageResource(R.drawable.unlocked_level3);
+            image_level1.setImageResource(R.drawable.abinadi);
         }
         if (faithPoints >= level3){
-            image_level1.setImageResource(R.drawable.unlocked_level3);
+            image_level1.setImageResource(R.drawable.abraham);
+        }
+        if (faithPoints >= level4){
+            image_level1.setImageResource(R.drawable.ammon);
+        }
+        if (faithPoints >= level5){
+            image_level1.setImageResource(R.drawable.anne);
+        }
+        if (faithPoints >= level6){
+            image_level1.setImageResource(R.drawable.daniel);
+        }
+        if (faithPoints >= level7){
+            image_level1.setImageResource(R.drawable.esther);
+        }
+        if (faithPoints >= level8){
+            image_level1.setImageResource(R.drawable.mormon);
+        }
+        if (faithPoints >= level9){
+            image_level1.setImageResource(R.drawable.moroni);
+        }
+        if (faithPoints >= level10){
+            image_level1.setImageResource(R.drawable.mosiah);
         }
 
 
@@ -59,6 +87,15 @@ public class CardActivity extends AppCompatActivity {
         level1 = 100;
         level2 = 200;
         level3 = 300;
+        level4 = 400;
+        level5 = 500;
+        level6 = 600;
+        level7 = 700;
+        level8 = 800;
+        level9 = 900;
+        level10 = 1000;
+        level11 = 1100;
+        level12 = 1200;
 
         changeImage();
     }
