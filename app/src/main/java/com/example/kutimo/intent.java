8170c91d327b285e657e8a4afd4ed3a32a228eac
timjo6789 <tgj2u2@gmail.com>
2020-12-07 11:24:54 -0700

    removing unused imports for intent class

--- app/src/main/java/com/example/kutimo/intent.java
+++ app/src/main/java/com/example/kutimo/intent.java
@@ -1,21 +1,10 @@
 package com.example.kutimo;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class intent extends AppCompatActivity {
 
