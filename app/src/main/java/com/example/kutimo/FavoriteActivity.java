ea543e46da5b6b6266911c6163df3982890e920c
timjo6789 <tgj2u2@gmail.com>
2020-12-07 11:05:15 -0700

    updating ScriptureItemView to be more specific

--- app/src/main/java/com/example/kutimo/FavoriteActivity.java
+++ app/src/main/java/com/example/kutimo/FavoriteActivity.java
@@ -2,14 +2,8 @@ package com.example.kutimo;
 
 import android.os.Bundle;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 import android.widget.LinearLayout;
 
 public class FavoriteActivity extends AppCompatActivity {
@@ -23,9 +17,9 @@ public class FavoriteActivity extends AppCompatActivity {
 
         for (int i = 0; i < 100; i++) {
             ScriptureItemView scriptureItemView = new ScriptureItemView(this);
-            scriptureItemView.setArtistText("Bob");
-            scriptureItemView.setButton("PRESS ME");
-            scriptureItemView.setTrackText("MUSIC time!");
+            scriptureItemView.setTitleText("Esther 8:9");
+            scriptureItemView.setButton("SHARE");
+            scriptureItemView.setQuoteText("Then were the king’s scribes called at that time in the third month, that is, the month Sivan, on the three and twentieth day thereof; and it was written according to all that Mordecai commanded unto the Jews, and to the lieutenants, and the deputies and rulers of the provinces which are from India unto Ethiopia, an hundred twenty and seven provinces, unto every province according to the writing thereof, and unto every people after their language, and to the Jews according to their writing, and according to their language.");
             linearLayout.addView(scriptureItemView);
         }
     }
