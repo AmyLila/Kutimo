3a2e159222675fdc7c5d0e124135b84e359c42bf
timjo6789 <tgj2u2@gmail.com>
2020-12-07 11:22:47 -0700

    removing ability to share image

--- app/src/main/java/com/example/kutimo/intent.java
+++ app/src/main/java/com/example/kutimo/intent.java
@@ -32,12 +32,6 @@ public class intent extends AppCompatActivity {
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if ("text/plain".equals(type)) {
                 handleSendText(intent); // Handle text being sent
-            } else if (type.startsWith("image/")) {
-                handleSendImage(intent); // Handle single image being sent
-            }
-        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
-            if (type.startsWith("image/")) {
-                handleSendMultipleImages(intent); // Handle multiple images being sent
             }
         } else {
             // Handle other intents, such as being started from the home screen
@@ -51,22 +45,6 @@ public class intent extends AppCompatActivity {
             Toast.makeText(this, sharedText, Toast.LENGTH_SHORT).show();
             EditText textView = (EditText) findViewById(R.id.intentResult);
             textView.setText(sharedText);
-        }
-    }
-
-    void handleSendImage(Intent intent) {
-        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        if (imageUri != null) {
-            Toast.makeText(this, "URI Image", Toast.LENGTH_SHORT).show();
-            // Update UI to reflect image being shared
-        }
-    }
-
-    void handleSendMultipleImages(Intent intent) {
-        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-        if (imageUris != null) {
-            Toast.makeText(this, "Multiple Images.", Toast.LENGTH_SHORT).show();
-            // Update UI to reflect multiple images being shared
         }
     }
 }
