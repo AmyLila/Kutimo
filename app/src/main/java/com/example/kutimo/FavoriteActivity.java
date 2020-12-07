55ec49272901887cb89e168edc5fb11b8906029a
timjo6789 <tgj2u2@gmail.com>
2020-12-07 11:18:15 -0700

    shorterning the example to prepare for save/load calls

--- app/src/main/java/com/example/kutimo/FavoriteActivity.java
+++ app/src/main/java/com/example/kutimo/FavoriteActivity.java
@@ -16,10 +16,7 @@ public class FavoriteActivity extends AppCompatActivity {
         LinearLayout linearLayout = findViewById(R.id.linear_layout);
 
         for (int i = 0; i < 100; i++) {
-            ScriptureItemView scriptureItemView = new ScriptureItemView(this);
-            scriptureItemView.setTitleText("Esther 8:9");
-            scriptureItemView.setButton("SHARE");
-            scriptureItemView.setQuoteText("Then were the king’s scribes called at that time in the third month, that is, the month Sivan, on the three and twentieth day thereof; and it was written according to all that Mordecai commanded unto the Jews, and to the lieutenants, and the deputies and rulers of the provinces which are from India unto Ethiopia, an hundred twenty and seven provinces, unto every province according to the writing thereof, and unto every people after their language, and to the Jews according to their writing, and according to their language.");
+            ScriptureItemView scriptureItemView = new ScriptureItemView(this, "Esther 8:9", "Then were the king’s scribes called at that time in the third month, that is, the month Sivan, on the three and twentieth day thereof; and it was written according to all that Mordecai commanded unto the Jews, and to the lieutenants, and the deputies and rulers of the provinces which are from India unto Ethiopia, an hundred twenty and seven provinces, unto every province according to the writing thereof, and unto every people after their language, and to the Jews according to their writing, and according to their language.", "SHARE");
             linearLayout.addView(scriptureItemView);
         }
     }
