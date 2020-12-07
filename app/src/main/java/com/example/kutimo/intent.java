2c00cf72a24994c1cac5dfea350bd73fd412cffe
timjo6789 <tgj2u2@gmail.com>
2020-12-07 11:57:15 -0700

    extracting gospel url for favorite activity

--- app/src/main/java/com/example/kutimo/intent.java
+++ app/src/main/java/com/example/kutimo/intent.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class intent extends AppCompatActivity {
+
+    protected String TAG_NAME = "intent";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,28 @@ public class intent extends AppCompatActivity {
         }
     }
 
+    private String getGospelUrl(String text){
+        String url_regex = "(https:\\/\\/www[.][-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|])";
+        Pattern p = Pattern.compile(url_regex, Pattern.MULTILINE);
+        Matcher m = p.matcher(text);
+
+        // Get the last url in case potential of url in content text.
+        String url_string = "";
+        while(m.find()) {
+            url_string = m.group();
+        }
+        return url_string;
+    }
+
     void handleSendText(Intent intent) {
         String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
         if (sharedText != null) {
+            String title = "";
+            String content = "";
+            String link = getGospelUrl(sharedText);
             Toast.makeText(this, sharedText, Toast.LENGTH_SHORT).show();
             EditText textView = (EditText) findViewById(R.id.intentResult);
+            // textView.setText(sharedText);
             textView.setText(sharedText);
         }
     }
