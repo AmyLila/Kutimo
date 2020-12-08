4976550207bfd005547048fec4401d6c974e6590
timjo6789 <tgj2u2@gmail.com>
2020-12-08 08:35:14 -0700

    improving loading and saving json array

--- app/src/main/java/com/example/kutimo/Data.java
+++ app/src/main/java/com/example/kutimo/Data.java
@@ -77,32 +77,3 @@ public class Data {
         return (JSONObject) new JSONParser().parse(loadString(shared_preference));
     }
 }
-
-    /*
-    public int faithPoints;
-    Save save;
-    public ArrayList<String> scriptures = new ArrayList<>();
-
-
-    public Data() {
-
-        faithPoints = 0;
-        faithPoints = save.retrieveFaithPoints();
-
-
-        scriptures.add("Hello");
-        scriptures.add("Hola");
-        scriptures.add("Bonjour");
-        scriptures.add("Ciao");
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Data{" +
-                "faithPoints=" + faithPoints +
-                ", scriptures=" + scriptures +
-                '}';
-    }
-     */
