50164ed1f60e6dccbed4dbb0a4a6abdc1ab13430
MeganDLS <megan.siguenza@gmail.com>
2020-12-09 10:43:26 -0700

    Adding TODAY string, getting rid of kutimo image for day selector, and implementing uniform colors.

--- app/src/main/java/com/example/kutimo/DatePicker.java
+++ app/src/main/java/com/example/kutimo/DatePicker.java
@@ -3,32 +3,49 @@ package com.example.kutimo;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.View;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.builders.DatePickerBuilder;
 import com.applandeo.materialcalendarview.exceptions.OutOfDateRangeException;
+import com.applandeo.materialcalendarview.listeners.OnSelectDateListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class DatePicker extends AppCompatActivity {
+//public static final int TODAY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        /**Kutimo image on just the current date
         List<EventDay> events = new ArrayList<>();
-
         Calendar calendar = Calendar.getInstance();
         events.add(new EventDay(calendar, R.drawable.kutimo));
 
         CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
         calendarView.setEvents(events);
+
+        //Setting a current date
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 7, 5);
+        try {
+            calendarView.setDate(calendar);
+        } catch (OutOfDateRangeException e) {
+            e.printStackTrace();
+        }
+
+        //Setting selected dates
+        List<Calendar> calendars = new ArrayList<>();
+        calendarView.setSelectedDates(calendars);**/
+
 
     }
 }
