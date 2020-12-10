0b20eb4a25654063666513a032043e8002386d3f
timjo6789 <tgj2u2@gmail.com>
2020-12-10 13:06:46 -0700

    changing single letter to make them work as float instead of int

--- app/src/main/java/com/example/kutimo/StorageKeys.java
+++ app/src/main/java/com/example/kutimo/StorageKeys.java
@@ -1,9 +1,11 @@
 package com.example.kutimo;
 
 public class StorageKeys {
-    protected static String FAITH_POINTS = "faith points";
-    protected static String MULTIPLIER = "faith multiplier";
+    protected static String FAITH_POINTS = "faith point";
+    protected static String MULTIPLIER = "faith multipliers";
     protected static String LEVEL_UP_POINTS = "level up points";
     protected static String CURRENT_LEVEL = "current level";
     protected static String SCRIPTURES = "favorite scriptures";
+    protected static String DATE = "calendar dates tracker";
+    protected static String CURRENT_DATE = "Current Date";
 }
