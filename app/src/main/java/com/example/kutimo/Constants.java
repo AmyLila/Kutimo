ef221d7877a51b0412a8861010ec3c863f6a0ca2
timjo6789 <tgj2u2@gmail.com>
2020-12-11 16:19:35 -0700

    updating string xml to reduce unneeded data and moving other data to constants

--- app/src/main/java/com/example/kutimo/Constants.java
+++ app/src/main/java/com/example/kutimo/Constants.java
@@ -1,6 +1,22 @@
 package com.example.kutimo;
 
 public class Constants {
-    static int[] LEVELS = {0, 500, 1000, 1500, 2000, 2500, 3500, 4000, 5000, 5500, 6000, 7000, 7500, 8000, 8500, 10_000};
+    static final int[] LEVELS = {0, 500, 1000, 1500, 2000, 2500, 3500, 4000, 5000, 5500, 6000, 7000, 7500, 8000, 8500, 10_000};
+
+    static final int[] CHRONOMETER_TRIGGER_TOAST = {1, 15, 30, 60, 120, 180, 300, 600, 900, 1800, 2700, 3600, 7200, 10800, 18000};
+    static final String[] CHRONOMETER_TOASTS = {"All efforts begin with desire.",
+            "It's been 15 sec. Feel the Spirit yet?",
+            "First Faith Point of the day!",
+            "These are times of faith, these are times of perseverance.",
+            "Believe. Love. Do.",
+            "Heaven is cheering you on!",
+            "He who reads it oftenest will like it best!",
+            "First I obey, then I understand!",
+            "Believing requires action.",
+            "We did not come this far to only come this far!",
+            "Decisions determine destiny!",
+            "Joy comes from and because of Him.",
+            "No one is destined to fail.",
+            "Well done, thou good and faithful servant."};
 
 }
