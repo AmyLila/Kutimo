4a94cef652f95c37f62fd0e743769f07dbc8c011
timjo6789 <tgj2u2@gmail.com>
2020-12-11 13:58:56 -0700

    adding constants file with ability to set level ranges

---
+++ app/src/main/java/com/example/kutimo/Constants.java
@@ -0,0 +1,6 @@
+package com.example.kutimo;
+
+public class Constants {
+    static int[] LEVELS = {0, 500, 1000, 1500, 2000, 2500, 3500, 4000, 5000, 5500, 6000, 7000, 7500, 8000, 8500, 10_000};
+
+}
