6957b83a537ab86b08a2befd08affb1d7be52d9f
timjo6789 <tgj2u2@gmail.com>
2020-12-11 14:04:54 -0700

    Simplifying and updating to work with GetLevel class

--- app/src/main/java/com/example/kutimo/CardActivity.java
+++ app/src/main/java/com/example/kutimo/CardActivity.java
@@ -7,13 +7,7 @@ import android.util.Log;
 import android.widget.ImageView;
 
 public class CardActivity extends AppCompatActivity {
-
-    private static final String TAG = "Card Activity";
-    private float faithPoints;
-    Data data;
-    ImageView[] image_views;
-    int[] level_ranges;
-    int[] image_ids;
+    private static final String TAG = "CardActivity";
 
     private int getImageIdByName(String name) {
         return getResources().getIdentifier(name, "drawable", this.getPackageName());
@@ -27,36 +21,11 @@ public class CardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
-        data = new Data(this);
-        faithPoints = data.loadFloat(StorageKeys.FAITH_POINTS);
-        Log.i(TAG, "faith points: " + faithPoints);
 
-        preload();
-
-        for (int i = 0; i < image_ids.length; i++)
-            if (faithPoints >= level_ranges[i])
-                image_views[i].setImageResource(image_ids[i]);
-    }
-
-    private void preload() {
-        int level = 500;
-        int image_names_total = 15;
-        String image_prefix_name = "image";
-
-        image_ids = new int[image_names_total];
-
-        for (int i = 0; i < image_names_total; i++)
-            image_ids[i] = getImageIdByName(image_prefix_name + (i + 1));
-
-        image_views = new ImageView[image_names_total];
-        for (int i = 0; i < image_names_total; i++)
-            image_views[i] = (ImageView) findViewById(getViewIdByName("image_level" + (i + 1)));
-
-
-        // Levels
-        level_ranges = new int[image_names_total];
-        int power = 1;
-        for (int i = 0; i < level_ranges.length; i++)
-            level_ranges[i] = level * (int) Math.pow(2, power);
+        float faithPoints = new Data(this).loadFloat(StorageKeys.FAITH_POINTS);
+        for (int i = 0; i < GetLevel.getCurrentLevel(faithPoints) - 1; i++) {
+            ImageView image_view = (ImageView) findViewById(getViewIdByName("image_level" + (i + 1)));
+            image_view.setImageResource(getImageIdByName("image" + (i + 1)));
+        }
     }
 }
