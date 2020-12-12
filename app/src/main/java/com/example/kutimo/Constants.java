8135a0c581789b5a3dce6da9c5ec3109a752fd27
timjo6789 <tgj2u2@gmail.com>
2020-12-12 11:33:09 -0700

    making a mini variable that handles church link prefix

--- app/src/main/java/com/example/kutimo/Constants.java
+++ app/src/main/java/com/example/kutimo/Constants.java
@@ -1,8 +1,9 @@
 package com.example.kutimo;
 
 public class Constants {
-    static final String COME_FOLLOW_ME_LINK = "https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-individuals-and-families-%s";
-    static final String SCRIPTURE_LINK = "https://www.churchofjesuschrist.org/study/scriptures/%s";
+    private String CHURCH_LINK = "https://www.churchofjesuschrist.org/study/";
+    static final String COME_FOLLOW_ME_LINK = CHURCH_LINK + "manual/come-follow-me-for-individuals-and-families-%s";
+    static final String SCRIPTURE_LINK = CHURCH_LINK + "scriptures/%s";
 
     static final int[] LEVELS = {0, 500, 1000, 1500, 2000, 2500, 3500, 4000, 5000, 5500, 6000, 7000, 7500, 8000, 8500, 10_000};
 
